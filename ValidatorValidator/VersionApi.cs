f2a2940af1b12f4be8fb5ca33fe4265ce0bd9c84
cdc-dpbrown <ita3@cdc.gov>
2019-08-28 11:14:06 -0400

    Added XUnit.

---
+++ ValidatorValidator/VersionApi.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace ValidatorValidator
+{
+    public class VersionApi
+    {
+        public VersionApi()
+        {
+
+        }
+
+        [Fact]
+        public void VersionValue()
+        {
+
+        }
+    }
+}
