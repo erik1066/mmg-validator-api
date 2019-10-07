62011084d98a2eeb89e72d73539fdc4ba4eec108
Mohammed lamtahri <vwm5@cdc.gov>
2019-10-07 15:54:31 -0400

    code clean-up

--- src/Models/Validator.cs
+++ src/Models/Validator.cs
@@ -55,9 +55,11 @@ namespace Cdc.mmg.validator.WebApi.Models
                     }
 
                     // validate data type 
-                    var List = ValidateDataType(FieldValue, segment, Item, ApiUri);
-                    ErrorList.AddRange(List);
-
+                    
+                  
+                        var errorsForRelatedElement1 = ValidateDataType(FieldValue, segment, Item, ApiUri);
+                        ErrorList.AddRange(errorsForRelatedElement1);
+                  
                     // validate related data elements (such as OBX-6)
 
                     foreach (var relatedElement in mmgElementList.Where(e => e.relatedDataElementId == Item.id))
@@ -86,16 +88,7 @@ namespace Cdc.mmg.validator.WebApi.Models
                     /* This logic can be used to implement checking for repeating data elements, but the other code in this file may need
                      some refactoring to get it integrated */
 
-                    var field = segment.Fields(5);
-
-                    if (field.HasRepetitions)
-                    {
-                        foreach (var repeatedField in field.Repetitions())
-                        {
-                            var errorsForRelatedElement = ValidateDataType(FieldValue, segment, Item, ApiUri);
-                            ErrorList.AddRange(errorsForRelatedElement);
-                        }
-                    }
+                   
 
                     //Validate codes using API
                 }
@@ -116,24 +109,28 @@ namespace Cdc.mmg.validator.WebApi.Models
         }
         public List<string> ValidateNK1(List<Segment> segList, List<DataElement> mmgElementList, string ApiUri)
         {
+            //NK1|1||MTH^Mother^HL70063|^^^22^71301^USA^^^22079||||||||||S^Single^HL70002||19940113||||||||||||2186-5^Not Hispanic or Latino^CDCREC|||||||2054-5^Black or African American^CDCREC
             List<string> List = new List<string>();
             return List;
 
         }
         public List<string> ValidateNTE(List<Segment> segList, List<DataElement> mmgElementList, string ApiUri)
         {
+            //NTE|1|L|Response to therapy is indicated by a > or =4-fold decrease in titer between pre and post treatment samples. However, a significant decrease in RPR titers may not occur for months to years following treatment, some patients may show persistent, low-level titers (e.g. serofast) despite adequate therapy.
             List<string> List = new List<string>();
             return List;
 
         }
         public List<string> ValidatePID(List<Segment> segList, List<DataElement> mmgElementList, string ApiUri)
         {
+            //PID|1||CONSYPH_TC01^^^SendAppName&2.16.840.1.114222.nnnn&ISO||~^^^^^^S||20130801|F||2054-5^Black or African American^CDCREC|^^^22^71301^^^^22079|||||||||||2186-5^Not Hispanic or Latino^CDCREC
             List<string> List = new List<string>();
             return List;
 
         }
         public List<string> ValidateMSH(List<Segment> segList, List<DataElement> mmgElementList, string ApiUri)
         {
+            //MSH|^~\&|SendAppName^2.16.840.1.114222.nnnn^ISO|Sending-Facility^2.16.840.1.114222.nnnn^ISO|PHINCDS^2.16.840.1.114222.4.3.2.10^ISO|PHIN^2.16.840.1.114222^ISO|20141225120030.1234-0500||ORU^R01^ORU_R01|CONSYPH_V1_0_TM_TC01|T|2.5.1|||||||||NOTF_ORU_v3.0^PHINProfileID^2.16.840.1.114222.4.10.3^ISO~Generic_MMG_V2.0^PHINMsgMapID^2.16.840.1.114222.4.10.4^ISO~CongenitalSyphilis_MMG_V1.0^PHINMsgMapID^2.16.840.1.114222.4.10.4^ISO
             List<string> List = new List<string>();
             return List;
 
@@ -146,6 +143,7 @@ namespace Cdc.mmg.validator.WebApi.Models
         }
         public List<string> ValidateOBR(List<Segment> segList, List<DataElement> mmgElementList, string ApiUri)
         {
+            //OBR|1||CONSYPH_TC01^SendAppName^2.16.840.1.114222.nnnn^ISO|68991-9^Epidemiologic Information^LN|||20130825170100|||||||||||||||20130825170100|||F||||||10316^Syphilis, Congenital^NND
             List<string> List = new List<string>();
             return List;
 
@@ -174,6 +172,30 @@ namespace Cdc.mmg.validator.WebApi.Models
                     {
                         ErrorList.Add("Error: Invalid numeric value. HL7 Identifier: " + FieldValue);
                     }
+                    break;
+                case "ST":  //Validate 
+
+                    var field = segment.Fields(5);
+
+                    if (field.HasRepetitions)
+                    {
+                        foreach (var repeatedField in field.Repetitions())
+                        {
+                            if (string.IsNullOrWhiteSpace(repeatedField.Value))
+                            {
+                                ErrorList.Add("Error: Invalid string value. HL7 Identifier: " + repeatedField.Value);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (string.IsNullOrWhiteSpace(FieldValue))
+                        {
+                            ErrorList.Add("Error: Invalid string value. HL7 Identifier: " + FieldValue);
+                        }
+
+                    }
+                 
                     break;
                 case "CWE":  //Validate codes using API
                     var ValueSetCode = Item.valueSetCode;
